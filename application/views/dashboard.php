ff21be852975f85dcda98494206177f4d833626b
Sasah24 <aisah30.ma@gmail.com>
2023-03-17 12:03:44 +0700

    absensi karyawan

---
+++ application/views/dashboard.php
@@ -0,0 +1,6 @@
+<div class="jumbotron bg-primary text-white">
+    <h4 class="my-0">Selamat datang di</h4>
+    <h1 class="display-4 my-0">Website Absensi Online</h1>
+    <hr class="my-4">
+    <p class="lead">Silahkan melakukan absensi dimenu Absen!!!</p>
+</div>
